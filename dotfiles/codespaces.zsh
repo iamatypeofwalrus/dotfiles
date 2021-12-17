bbdc32879d4103831f8479d52048c99cbc221efd
Joe Feeney <joe@glasswaves.co>
2021-12-17 01:24:10 +0000

    update dotfiles

---
+++ dotfiles/codespaces.zsh
@@ -0,0 +1,4 @@
+export EDITOR=code
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43' # ls colors on Ubuntu that match Mac OS
+
+alias ls='ls --color=auto' # couldn't figure out how to do it with just the env variables. this was pretty quick
