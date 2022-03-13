87de1791c590442bac1634b82a9aec191ff06b75
Joe Feeney <iamatypeofwalrus@users.noreply.github.com>
2022-03-12 18:33:30 -0800

    Update codespaces.zsh

--- dotfiles/codespaces.zsh
+++ dotfiles/codespaces.zsh
@@ -1,4 +1,5 @@
 export EDITOR='code --wait'
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43' # ls colors on Ubuntu that match Mac OS
+export PATH=/usr/local/go/bin:$PATH # prefer the version that's installed here as it's the one the Golang extension will use
 
 alias ls='ls --color=auto' # couldn't figure out how to do it with just the env variables. this was pretty quick
