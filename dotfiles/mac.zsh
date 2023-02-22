c93028e951ec59f506ce3763b3c29cce48128848
Joe Feeney <joe@glasswaves.co>
2023-02-21 16:47:37 -0800

    update conda bin

--- dotfiles/mac.zsh
+++ dotfiles/mac.zsh
@@ -7,7 +7,7 @@ export PATH=/usr/local/bin:$PATH
 export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin":$PATH # subl
 export PATH="/opt/homebrew/bin":$PATH
 
-export PATH="/Users/jfeeney/opt/anaconda3/bin:$PATH"
+export PATH="$HOME/opt/anaconda3/bin:$PATH"
 export PATH=~/.local/bin:$PATH # python user install
 export PATH="/opt/homebrew/opt/libpq/bin:$PATH" # postgresql client
 
